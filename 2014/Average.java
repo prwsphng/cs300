73d1205e0411bc908125194e47f0feb8f5cc800d
prae <121804853+prwsphng@users.noreply.github.com>
2023-12-17 01:12:27 +0700

    Add files via upload

---
+++ 2014/Average.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Average{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // input size of array
+        int n = scan.nextInt();
+
+        // create arr 
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scan.nextInt();
+        }
+
+        // index pf 30%
+        double temp = Math.ceil(n*0.3);
+
+        // sorting 
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for(int i=0; i< (int)temp; i++){
+            sum += arr[i];
+        }
+        int result = sum / (int)temp;
+
+        System.out.println(result);
+        scan.close();
+    }
+}
